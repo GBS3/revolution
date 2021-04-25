19a43bc003de4b11e1d1c0d67474ac5d31611538
GBS3 <70548443+GBS3@users.noreply.github.com>
2021-04-24 23:09:42 -0500

    Update Spinner to work with Color

--- revolution/spinner.py
+++ revolution/spinner.py
@@ -1,3 +1,7 @@
+from .colors import Color
+from .colors import wrap_iterable_elements_with_color
+
+
 class Spinner:
     spinners = {
         'classic': (['|', '/', '-', '\\'], 0.1),
@@ -14,23 +18,46 @@ class Spinner:
     }
 
     def __iter__(self):
+        # Sets internal `_count` to 0 each time we begin a for-based loop:
         self._count = 0
         return self
 
     def __next__(self):
+        # If there are still animations to print, continue printing them:
         if self._count < self._spinner_len:
-            spinner_instance = self._spinner[self._count]
+            spinner_frame = self._spinner[self._count]
             self._count += 1
-            return spinner_instance
+            return spinner_frame
+
+        # Otherwise, quit:
         raise StopIteration
 
-    def __init__(self, style, interval=None):
+    def __init__(self, style=None, interval=None, color=None):
+        # If style is empty, default to 'classic':
         self._style = style or 'classic'
 
-        try:
-            spinner_group = self.spinners[self._style]
-        except KeyError:
-            spinner_group = self.spinners['classic']
-        self._spinner, self.interval = spinner_group
+        while True:
+            try:
+                # `spinner_tuple` is a tuple containing a list of spinner animations
+                # and an interval indicating how often to refresh the animation:
+                spinner_tuple = self.spinners[self._style]
+                break
+            # If the given style doesn't exist, default to 'classic':
+            except KeyError:
+                self._style = 'classic'
 
+        # `self.interval` is intended to be public:
+        self._spinner, self.interval = spinner_tuple
+
+        # If a color is provided, we should color the animation frames:
+        if color:
+            color_obj = Color(color)
+            self._spinner = wrap_iterable_elements_with_color(
+                self._spinner, color_obj.color)
+        # If user specifies an interval, use that instead:
+        if interval:
+            self.interval = interval
+
+        # `_spinner_len` is used internally to indicate when the spinner animation
+        # should fully reset:
         self._spinner_len = len(self._spinner)
