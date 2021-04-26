11ba46503a3c6d854d17ddd64b737ad9c0bac97b
GBS3 <70548443+GBS3@users.noreply.github.com>
2021-04-26 10:15:57 -0500

    Change development status

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 classifiers = [
-    'Development Status :: 5 - Production/Stable',
+    'Development Status :: 4 - Beta',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
